c9df8281006d936ebe8e98f47b4d0588405ef404
zhouxingrong <zhouxingrong@putao.com>
2018-01-05 15:41:12 +0800

    fixed  code

---
+++ sharecomponentkotlin/src/main/runalone/java/com/luojilab/share/kotlin/runalone/application/ShareKotlinApplication.kt
@@ -0,0 +1,20 @@
+package com.luojilab.share.kotlin.runalone.application
+
+import android.app.Application
+
+/**
+ * Description:
+ * Copyright  : Copyright (c) 2018
+ * Email      : jusenr@163.com
+ * Author     : Jusenr
+ * Date       : 2018/01/05
+ * Time       : 14:52
+ * Project    ：DDComponentForAndroid.
+ */
+class ShareKotlinApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
